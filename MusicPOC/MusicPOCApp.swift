cd4fe78cc079d97284c3b23153bc41db3b0ed164
Francisco Soares <fmssn@cin.ufpe.br>
2026-04-27 14:39:24 -0300

    Initial Commit

---
+++ MusicPOC/MusicPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPOCApp.swift
+//  MusicPOC
+//
+//  Created by Francisco Miranda Soares on 27/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MusicPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
